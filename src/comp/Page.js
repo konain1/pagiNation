00dd414c7b4116cdfaee47d6341fea9e192a4a49
konain1 <kaunainkasab@gmail.com>
2023-08-20 18:17:18 +0530

    nextButtonbugFixed

--- src/comp/Page.js
+++ src/comp/Page.js
@@ -11,15 +11,16 @@ function Page({apiData}) {
   const [startIndex,setStartIndex] = useState(1)
 
   const [pageNo,setPageNo] = useState(1)
+
+
 const NextHandler = ()=>{
 
+ 
   setStartIndex(startIndex+1)
 }
 const PrevHandler = ()=>{
-  if(setStartIndex < 1){
-    document.getElementById('prev').style.display = 'none'
-  }
-
+  
+ 
   setStartIndex(startIndex-1)
 }
 
@@ -32,7 +33,9 @@ const pageNoHandler = (event)=>{
   return (
     <>
     <div className='photos'>
+
     {apiData.slice(startIndex *10 -10,startIndex*10).map((item,key)=>{
+
         return ( <div key={item.id} > <img src={item.thumbnailUrl} className='photo'/><p>{item.id}</p></div>)
       })}
 
@@ -44,14 +47,14 @@ const pageNoHandler = (event)=>{
 
     }
     {
-        startIndex < apiData.length-1 ?  <button className='nextPrev' onClick={NextHandler}>Next</button> : 'plz Press Prev Button'
+        startIndex < (apiData.length / 10 ) -1 ?  <button  id='next' className='nextPrev' onClick={NextHandler}>Next</button> : 'plz Press Prev Button'
      }
 
      <div className='pageNo'>
     
 
      
-      {apiData.slice(pageNo,apiData.length / 10).map((item,key)=>{
+      {apiData.slice(1,apiData.length / 10).map((item,key)=>{
         return <button onClick={(e)=>pageNoHandler (e)} className={startIndex == item.id -1 ?'currentItem':''}  key={item.id} >{item.id -1  }</button>
       })} 
 
